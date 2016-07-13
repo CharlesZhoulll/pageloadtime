55ea4c8b27c029ba08e092420e6a087e593a73ec
charleszhou327@gmail.com <charleszhou327@gmail.com>
2016-07-13 11:16:27 -0400

    fix lots of bugs, now fairly work

--- app/src/main/java/com/example/charles/pltmeasurement/PLTmeasurActivity.java
+++ app/src/main/java/com/example/charles/pltmeasurement/PLTmeasurActivity.java
@@ -1,6 +1,8 @@
 package com.example.charles.pltmeasurement;
 
 import android.graphics.Bitmap;
+import android.os.Looper;
+import android.print.PageRange;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,37 +17,46 @@ import android.widget.Button;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.concurrent.RunnableFuture;
 
 public class PLTmeasurActivity extends AppCompatActivity {
 
     private static final String TAG = "PLTmeasurement";
+    private static final int TIMEOUT_COUNTER = 30000;  // Stop loading more results after 30 second until the last website is loaded
+    private static final int LOADING_INTERVAL = 2000;
     private static final ArrayList<String> urlList = new ArrayList<String>();
-    private static ArrayList<String> measurementResults = new ArrayList<String>();
-    private static String USER_AGENT="Mozilla/5.0 (Linux; U; Android 4.3; en-us; SCH-I535 Build/JSS15J)" +
-            " AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30";
     private static final String js_forNT = "javascript:(\n function() { \n"
-            + "var result='';\n"
+            + "setTimeout(function(){var result='';\n"
             + "var perfOBJ = performance.timing;\n"
             + "var PLT = perfOBJ.loadEventEnd - perfOBJ.navigationStart;\n"
             + "var PIT = perfOBJ.domComplete - perfOBJ.navigationStart;\n"
             + "var PTT = perfOBJ.responseEnd - perfOBJ.navigationStart;\n"
             + "result = PTT + ',' + PIT + ',' + PLT;\n"
-            + "console.log(result);\n"
-            + "for (var prop in perfOBJ){\n"
-            + "console.log(prop + ':' + perfOBJ[prop]); }\n"
+            + "console.log('PLTresults:' + result);}, 500);\n"
+            //+ "console.log('PLTresults:' + result);\n"
+            //+ "for (var prop in perfOBJ){\n"
+            //+ "console.log(prop + ':' + perfOBJ[prop]); }}, 500);\n"
             //+ "console.log('Redirection count:' + performance.navigation.redirectCount);\n"
             + " })()\n";
-
+    private static boolean TIMEOUT = false;  // To see if expired
+    private static HashMap<String, String> measurementResults = new HashMap<String, String>();
+    private static String USER_AGENT = "Mozilla/5.0 (Linux; U; Android 4.3; en-us; SCH-I535 Build/JSS15J)" +
+            " AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30";
     private int currentUrlIndex = 0;
     private int totalURLNumber = 0;
 
-    private boolean readUrlFromFile(String urllist){
+    private boolean readUrlFromFile(String urllist) {
         try {
             BufferedReader inputReader = new BufferedReader(new InputStreamReader(getAssets().open(urllist)));
             String mLine;
-            while ((mLine = inputReader.readLine()) != null){
-                Log.d(TAG, "Read URL:" + mLine);
+            while ((mLine = inputReader.readLine()) != null) {
+                if (mLine.isEmpty())
+                    continue;
+                //Log.d(TAG, "Read URL:" + mLine);
                 if (!mLine.startsWith("http://"))
                     mLine = "http://www." + mLine;
                 urlList.add(mLine);
@@ -58,67 +69,150 @@ public class PLTmeasurActivity extends AppCompatActivity {
         return (totalURLNumber > 0);
     }
 
+    private String parseUrl(String url) {
+        try {
+            URL url_parsed = new URL(url);
+            return url_parsed.getHost();
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Cannot parse URL " + url);
+            return url;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Read all URLs
-        if(!readUrlFromFile("website")){
+        if (!readUrlFromFile("website")) {
             return;
         }
+        //Log.d(TAG, js_forNT);
         final Button start = (Button) findViewById(R.id.buttonStart);
-        start.setOnClickListener(new View.OnClickListener(){
+        start.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                WebView mywebview = (WebView) findViewById(R.id.myWebView);
+                final WebView mywebview = (WebView) findViewById(R.id.myWebView);
                 // Set up webview client
                 mywebview.setWebViewClient(new WebViewClient() {
+
+                    Boolean loadingFinished = true;
+                    Boolean redirect = false;
+
                     @Override
                     public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                        //Log.d(TAG, url + " shouldOverrideUrlLoading!!!");
+                        if (!loadingFinished) {
+                            redirect = true;
+                        }
+                        loadingFinished = false;
                         view.loadUrl(url);
                         return true;
                     }
 
                     @Override
-                    public void onPageFinished(WebView view, String url){
-                        super.onPageFinished(view, url);
-                        Log.d(TAG, url + " finish loading !");
-                        new Handler() {
-                            private WebView view;
-                            public void init(WebView v) {
-                                view = v;
-                                post(new Runnable () {
-                                    @Override
-                                    public void run() {
-                                        view.loadUrl(js_forNT);
-                                        if ((currentUrlIndex + 1) < totalURLNumber)
-                                        {
-                                            currentUrlIndex += 1;
-                                            view.loadUrl(urlList.get(currentUrlIndex));
-                                        }
-                                    }
-                                });
+                    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                        //super.onPageStarted(view, url, favicon);
+                        //Log.d(TAG, url + " onPageStarted!!!");
+                        loadingFinished = false;
+                    }
+
+
+                    @Override
+                    public void onPageFinished(WebView view, String url) {
+                        //super.onPageFinished(view, url);
+                        Log.d(TAG, url + " onPageFinished!!!");
+                        if (!redirect) {
+                            loadingFinished = true;
+                        }
+                        if (loadingFinished && !redirect) {
+                            //Log.d(TAG, url + " Finished Loading!!!");
+                            view.loadUrl(js_forNT);
+                            // if in measured results, do not loadNext, we have seen this before(so the next page has been loaded)
+                            // if not in measured results, we initial it in measured result,  load next
+
+/*                            String parsedURL = parseUrl(url);
+                            if (measurementResults.get(parsedURL) == null) {
+
+                                Log.d(TAG, "Did not found: " + parsedURL);
+                                measurementResults.put(parsedURL, "0,0,0");
+                                loadNext();
                             }
-                        }.init(view);
+                            else {
+                                Log.d(TAG, "Found: " + parsedURL);
+                            }*/
+                        } else {
+                            redirect = false;
+                        }
                     }
                 });
                 // set up webchromeclient
-                mywebview.setWebChromeClient(new WebChromeClient(){
+                mywebview.setWebChromeClient(new WebChromeClient() {
+
+                    private boolean handleMessage(String message, String host) {
+                        if (message.startsWith("PLTresults")) {
+                            message = message.substring(11, message.length());
+                            String[] separated = message.split(",");
+                            try {
+                                double ptt = Double.parseDouble(separated[0]);
+                                double pit = Double.parseDouble(separated[1]);
+                                double plt = Double.parseDouble(separated[2]);
+                                if (ptt < 0 ) ptt = 0;
+                                if (pit < 0 ) pit = 0;
+                                if (plt < 0 ) plt = 0;
+                                // Otherwise no valid data is collected
+
+                                measurementResults.put(host, message);
+                                return true;
+                            } catch (NumberFormatException e) {
+                                Log.e(TAG, "Cannot parse " + message);
+                                return false;
+                            }
+                        }
+                        return false;
+                    }
+
+                    private void loadNext() {
+                        Handler handler = new Handler();
+
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                if ((currentUrlIndex + 1) < totalURLNumber) {
+                                    currentUrlIndex += 1;
+                                    String handingUrl = urlList.get(currentUrlIndex);
+                                    Log.d(TAG, (currentUrlIndex + 1) + "'s url: " + handingUrl);
+                                    mywebview.loadUrl(handingUrl);
+                                } else {
+                                    if (TIMEOUT == false) {
+                                        TIMEOUT = true;
+                                        Log.d(TAG, "--------------Results summary-------------");
+                                        for (String url : measurementResults.keySet()) {
+                                            Log.d(TAG, url + " " + measurementResults.get(url));
+                                        }
+                                        Log.d(TAG, "----------------Summary end---------------");
+                                    }
+                                }
+                            }
+                        }, LOADING_INTERVAL);
+                    }
+
                     @Override
                     public void onConsoleMessage(String message, int lineNumber, String sourceID) {
-                        String[] separated = message.split(",");
-                        //double ptt = Double.parseDouble(separated[0]);
-                        //double pit = Double.parseDouble(separated[1]);
-                        //double plt = Double.parseDouble(separated[2]);
-                        // Otherwise no valid data is collected
-
                         Log.d(TAG, message);
-                        //if ((ptt> 0) && (pit > 0) && (plt > 0))
-                        //{
-                        //    Log.d(TAG, "Got valid data !");
-                        //    measurementResults.add(message);
-                        //}
+                        if (!TIMEOUT) {
+                            // You can have two choice: only load next page when last page is correctly handled
+                            // Or you can ignore it.. by remove the if condition
+                            String fullURL = mywebview.getUrl();
+                            if (fullURL != null) {
+                                Log.d(TAG, fullURL);
+                                String host = parseUrl(fullURL);
+                                if ((measurementResults.get(host) == null) && handleMessage(message, host))
+                                    loadNext();
+                            }
+                        }
                     }
                 });
+
 
                 // Set web setting
                 WebSettings webSettings = mywebview.getSettings();
@@ -126,7 +220,10 @@ public class PLTmeasurActivity extends AppCompatActivity {
                 webSettings.setAppCacheEnabled(false);
                 webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
                 webSettings.setUserAgentString(USER_AGENT);
-                mywebview.loadUrl(urlList.get(currentUrlIndex));
+
+                String handlingUrl = urlList.get(currentUrlIndex);
+                mywebview.loadUrl(handlingUrl);
+                Log.d(TAG, (currentUrlIndex + 1) + "'s url: " + handlingUrl);
             }
         });
     }
